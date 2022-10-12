58fa4850cd350897ef2ff36e4d7e09f6ff7f468f
rmrlangford <langfordrmr@gmail.com>
2022-10-12 09:25:01 +0200

    Update supserset-steps.js

--- test/features/steps/supserset-steps.js
+++ test/features/steps/supserset-steps.js
@@ -2,13 +2,11 @@ const { Then, When } = require('@cucumber/cucumber')
 const { expect } = require('chai')
 const { getSupsersetChart } = require('../helpers/api-helpers')
 
-When('I check Superset for chart data using the following', { timeout: 10 * 1000 }, async function (table) {
+When('I check Superset for chart data using the following', async function (table) {
     const params = {}
     table.hashes().forEach(hash => {
         params[hash.field] = hash.value
     })
-
-    await new Promise(r => setTimeout(r, 5000));
 
     const { data } = await getSupsersetChart(params)
     this.output = data
