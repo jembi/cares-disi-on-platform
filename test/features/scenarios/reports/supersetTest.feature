e43dd050d53a9da0c602bc21051454d2dc4166d8
rmrlangford <langfordrmr@gmail.com>
2022-10-13 13:42:00 +0200

    Updated

--- test/features/scenarios/reports/supersetTest.feature
+++ test/features/scenarios/reports/supersetTest.feature
@@ -2,18 +2,18 @@ Feature: CHART TEST
     Scenario: TEST
         And I check Superset for chart data using the following
             | field | value |
-            | ID    | 1     |
+            | ID    | 17    |
 
-        Then there should be a result identified by "obs.diagnostic_pcr_test_result" of "1662"
+        Then there should be a result identified by "obs.diagnostic_pcr_test_result" of "2838"
 
         And I check Superset for chart data using the following
             | field | value |
-            | ID    | 12    |
+            | ID    | 10    |
 
-        Then there should be a result identified by "f.city" of "Report 3A City" with the following fields and values
+        Then there should be a result identified by "p.patient_key_population" of "PWUD" with the following fields and values
             | field | value |
-            | count | 552   |
+            | count | 150   |
 
-        Then there should be a result identified by "f.city" of "Report 1A City" with the following fields and values
+        Then there should be a result identified by "p.patient_key_population" of "MSW" with the following fields and values
             | field | value |
-            | count | 473   |
+            | count | 134   |
--- test/features/steps/supserset-steps.js
+++ test/features/steps/supserset-steps.js
@@ -2,6 +2,18 @@ const { Then, When } = require('@cucumber/cucumber')
 const { expect } = require('chai')
 const { getSupsersetChart } = require('../helpers/api-helpers')
 
+function verifyResultRowExistsWithCorrectResultValue(resultField, resultValue, outputData) {
+    const res = outputData.result[0].data.find((r) => {
+        for (key in r) {
+            const chartElementKeyCleaned = String(key).replace("COUNT(", "").replace(")", "")
+
+            return resultField == chartElementKeyCleaned && r[key] == resultValue
+        }
+    });
+
+    return res;
+}
+
 When('I check Superset for chart data using the following', async function (table) {
     const params = {}
     table.hashes().forEach(hash => {
@@ -16,28 +28,14 @@ Then('there should be a result identified by {string} of {string}', function (fi
     const chartData = this.output.result.find(r => r['data'] != [])
     expect(chartData, 'Could not find chart data').to.not.be.undefined
 
-    const res = this.output.result[0].data.find((r) => {
-        for (key in r) {
-            const chartElementKeyCleaned = String(key).replace("COUNT(", "").replace(")", "")
-
-            return field == chartElementKeyCleaned && r[key] == value
-        }
-    });
-    expect(res, 'The expected result does not match the actual result').to.not.be.undefined
+    expect(verifyResultRowExistsWithCorrectResultValue(field, value, this.output), 'The expected result does not match the actual result').to.not.be.undefined
 })
 
 Then('there should be a result identified by {string} of {string} with the following fields and values', function (field, value, table) {
     const chartData = this.output.result.find(r => r['data'] != [])
     expect(chartData, 'Could not find chart data').to.not.be.undefined
 
-    const res = this.output.result[0].data.find((r) => {
-        for (key in r) {
-            const chartElementKeyCleaned = String(key).replace("COUNT(", "").replace(")", "")
-
-            return field == chartElementKeyCleaned && r[key] == value
-        }
-    });
-    expect(res, 'The expected result does not match the actual result').to.not.be.undefined
+    expect(verifyResultRowExistsWithCorrectResultValue(field, value, this.output), 'The expected result does not match the actual result').to.not.be.undefined
 
     this.output.result.find((r) => {
         table.hashes().forEach(hash => {
